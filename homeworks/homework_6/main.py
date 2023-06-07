512f5bea85f57fa34f218a092faea9779b45ea73
leonid-korolev <102927981+leonid-korolev@users.noreply.github.com>
2023-06-07 22:59:30 +0300

    Delete homeworks/homework_6 directory

--- homeworks/homework_6/main.py
+++
@@ -1,8 +0,0 @@
-from chess_module import eight_queens 
-from input_coordinates import input_coord
-from random_coordinates import random_coord
-
-
-if __name__ == '__main__':
-    #print(eight_queens(input_coord()))
-    print(eight_queens(random_coord()))
